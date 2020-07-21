6e9bd3bece1928fb01b0f73e1a54df62a64d38d7
Pranav Mittal <pranavmittal611@gmail.com>
2020-07-21 14:18:22 +0530

    Update and rename PyTricks.py to python/PyTricks.py

--- python/PyTricks.py
+++ python/PyTricks.py
@@ -29,28 +29,15 @@ time.ctime(seconds) gives local time formatted
 time.asctime(time.gmtime(seconds)) gives GMT time formatted
 ============================================================
 print('Python is\r123456') why not 123456 is
-len type id
-try except else finally
 stringname.isnumeric() will return True or False
 round=(int(avg*100))/100 #truncates to two decimal points
 n[::-1] reverses string n
 isinstance(object, classinfo) returns true or false
-return and global
-break pass continue
-append and extend
-abs(m-n) #absolute value
 =================================
-m,n=input().split()
-m,n=map(int,input().split())
-m,n=list(map(int,input().split()))
-=================================
-sep and end
-min max count sum sort
 math.gcd(a, b) or Euclid's Alg or Recursion        '
 ===========================
 print(*n, sep=' ')
 print(' '.join(n))
-print(n[x], end=' ') x in range
 ===========================
 input().strip()
 f, *l = input().split() //f is first input, other inputs in list l
